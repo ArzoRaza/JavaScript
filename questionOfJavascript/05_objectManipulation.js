061a6e3bb8e32908d8e93826c4d91c939ddaa097
ArzoRaza <ccoding76@gmail.com>
2024-05-12 19:14:46 +0530

    Object manipulation

---
+++ questionOfJavascript/05_objectManipulation.js
@@ -0,0 +1,12 @@
+let person = {
+    Name : "Akbar", 
+    Age: 22, 
+    City: "Punjab"
+}
+function printValues(person){
+    name: ('name: ' + person.name);
+    age: ('age: ' + person.age);
+    city: ('city: ' +person.city);
+}
+printValues(person);
+console.log(person);
