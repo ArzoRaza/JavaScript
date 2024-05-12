0005a06df7a2a1220ded01c1fc46d542dcfa7dd3
ArzoRaza <ccoding76@gmail.com>
2024-05-12 16:12:16 +0530

    it file all about practice of JavaScript language

---
+++ questionOfJavascript/conditional_statement.js
@@ -0,0 +1,12 @@
+function checkSing(number){
+    if(number>0){
+        return "positive";
+    }
+    else if(number < 0){
+        return "negative";
+    }
+    else(number == 0)
+        return "zero"
+    
+}
+console.log(checkSing(0))
